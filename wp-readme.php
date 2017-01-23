4d666515f9165b017e656b1736eb7299e8be81ed
fumikito <guy@hametuha.com>
2017-01-24 00:43:11 +0900

    Initial commit

---
+++ wp-readme.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Generate readme.txt from Github's README.md
+ *
+ * @author fumikito
+ * @version 1.0.0
+ * @see https://github.com/fumikito/wp-readme
+ */
+
+$file = false;
+
+// Try file
+foreach ( [ './readme.md', './README.md' ] as $file_name ) {
+	if ( file_exists( $file_name ) ) {
+		$file = $file_name;
+		break;
+	}
+}
+
+// File not found.
+if ( ! $file ) {
+	echo '[Error] No README.md in current directory.'.PHP_EOL;
+	exit(1);
+}
+
+echo "readme.md found...\n";
+
+$string = file_get_contents( $file );
+
+// Replace headers
+$string = preg_replace_callback( '/^(#+)\s+(.*)/mu', function( $match ) {
+	$length = strlen( $match[ 1 ] );
+	$sep = '';
+	for ( $i = 1, $l = 3 - ( $length - 1 ); $i <= $l; $i++ ) {
+		$sep .= '=';
+	}
+	return "{$sep} {$match[2]} {$sep}";
+}, $string );
+
+
+//保存
+if ( ! file_put_contents( './readme.txt', $string ) ) {
+	echo "[Error] Failed to save readme.txt.".PHP_EOL;
+	exit(1);
+}
+
+echo "readme.txt generated successfully!".PHP_EOL;
